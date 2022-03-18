018919d3b4491ec08f49cb9f3919c1cf59f3178c
topgrade691028 <joshua.lee.gladiator@gmail.com>
2022-03-19 07:08:46 +0800

    feat: configuration project structur

---
+++ src/layouts/AppLayout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AppLayout: React.FC = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export default AppLayout;
---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import type { AppProps } from "next/app";
+
+import AppLayout from "../layouts/AppLayout";
+
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <AppLayout>
+      <Component {...pageProps} />
+    </AppLayout>
+  );
+}
+
+export default MyApp;
