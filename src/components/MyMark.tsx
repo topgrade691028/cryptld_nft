3044248b1f58fb15e5ccb8be433ef5f2063b71d1
topStar <ninessander720@gmail.com>
2024-09-12 05:02:46 -0700

    fix mark

--- src/components/MyMark.tsx
+++ src/components/MyMark.tsx
@@ -15,7 +15,7 @@ const Wrapper = styled.div`
 export const MyMark: React.FC = () => {
   return (
     <Wrapper>
-      <img src="/moses.png" alt="my_mark" />
+      {/* <img src="/moses.png" alt="my_mark" /> */}
     </Wrapper>
   );
 };
